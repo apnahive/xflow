e498706d14380afad17a39c2ed3a5ec9800f2e31
unknown <dev.sadiquee@gmail.com>
2019-06-30 00:31:00 +0530

    commit 29062019 4

--- resources/views/vendor/sweetalert/alert.blade.php
+++ resources/views/vendor/sweetalert/alert.blade.php
@@ -1,4 +1,4 @@
-<!-- <script src="{{ asset('vendor/sweetalert/sweetalert.all.js') }}"></script> -->
+<script src="{{ asset('vendor/sweetalert/sweetalert.all.js') }}"></script>
 @if (Session::has('alert.config'))
     <script>
         Swal.fire({!! Session::pull('alert.config') !!});
